f9c3d8ccfb4ce12eddf01312c6a3198d7fa21e81
sandy <sandykarinebraz@gmail.com>
2024-08-06 14:24:02 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);
+  }function setup() {
+    createCanvas(400, 400);
+    background("")
+  
+    textFont("Comic Sans MS");
+    textSize(150)
+    textAlign(CENTER,CENTER);
+  
+  }
+  
+  function draw(){
+    text("sandy",200,200)
+  }
